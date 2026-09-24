08511ba8d1247628d7bd53328e9b5bf2bac6b64d
fengwang001 <1240156853@qq.com>
2026-09-24 13:24:54 +0800

    gsb: A artifact for ontology-157

---
+++ cmd/demo/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	pass, total := 0, 0
+	check := func(name string, ok bool) {
+		total++
+		if ok {
+			pass++
+			fmt.Println("OK  ", name)
+		} else {
+			fmt.Println("FAIL", name)
+		}
+	}
+	_ = check
+	fmt.Printf("TOTAL %d/%d\n", pass, total)
+}
